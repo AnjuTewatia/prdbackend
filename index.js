f852ef4121d4c4df299399047885f04f76e56990
AnjuTewatia <jeeveshtewatia2@gmail.com>
2023-02-09 15:19:48 +0530

    anju

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 require("dotenv").config();
-const express = require('express');
-const connect = require('./src/config/db');
-const cartrouter = require("./src/features/Cart/cart.route")
-const userrouter = require("./src/features/Auth/user.route")
-const productrouter = require("./src/features/Product/product.route")
+const express = require("express");
+const connect = require("./src/config/db");
+const cartrouter = require("./src/features/Cart/cart.route");
+const userrouter = require("./src/features/Auth/user.route");
+const productrouter = require("./src/features/Product/product.route");
 const PORT = process.env.PORT || 8080;
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 app.use(cors({ origin: "*" }));
-app.use(express.json())
+app.use(express.json());
 
-// user router 
 app.use("/user", userrouter);
 // product router
-app.use("/product", productrouter)
-// cart router
-app.use("/cart", cartrouter)
+app.use("/product", productrouter);
 
-// Default Response
+app.use("/cart", cartrouter);
+
 app.get("", async (req, res) => {
-    try {
-        res.sendFile(__dirname + '/src/utils/index.html');
-    } catch (error) {
-        res.send(error.message)
-    }
-})
-// connection with mongodb
+  try {
+    res.sendFile(__dirname + "/src/utils/index.html");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
 app.listen(PORT, async (req, res) => {
-    try {
-        await connect();
-        console.log("Connected to mongodb")
-    } catch (error) {
-        console.log(error);
-    }
-    console.log(`http://localhost:${PORT}`);
+  try {
+    await connect();
+    console.log("Connected to mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`http://localhost:${PORT}`);
 });
